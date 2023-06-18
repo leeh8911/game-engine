b47bc984f4f5e5261f9ba3ae8a592e7645f0f82e
sangwon lee <leeh8911@gmail.com>
2023-06-19 05:11:48 +0900

    update premake

---
+++ scripts/Win-GenProject.bat
@@ -0,0 +1,5 @@
+﻿@REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
+pushd ..\
+call 3rdparty\premake\premake5.exe vs2022 premake5.lua
+popd
+PAUSE
--- vs-make.bat
+++
@@ -1,2 +0,0 @@
-﻿@REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
-call premake5 vs2022 premake5.lua
