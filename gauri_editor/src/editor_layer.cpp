33b3d33a8cf394ce8dedcc77f4a0a1cabe79e2c4
sangwon lee <leeh8911@gmail.com>
2023-07-16 13:52:43 +0900

    update gauri

--- gauri_editor/src/editor_layer.cpp
+++ gauri_editor/src/editor_layer.cpp
@@ -41,6 +41,8 @@ void EditorLayer::OnAttach()
       public:
         void OnCreate()
         {
+            auto &transform = GetComponent<TransformComponent>().Transform;
+            transform[3][0] = rand() % 10 - 5.0f;
         }
         void OnDestroy()
         {
@@ -70,6 +72,7 @@ void EditorLayer::OnAttach()
     };
 
     m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+    m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 }
 
 void EditorLayer::OnDetach()
