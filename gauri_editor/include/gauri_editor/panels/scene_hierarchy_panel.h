b75452ddd51ed24a64d4c05845414bcea36f4480
sangwon lee <leeh8911@gmail.com>
2023-07-17 10:39:16 +0900

    property panel

--- gauri_editor/include/gauri_editor/panels/scene_hierarchy_panel.h
+++ gauri_editor/include/gauri_editor/panels/scene_hierarchy_panel.h
@@ -18,6 +18,7 @@ class SceneHierarchyPanel
 
   private:
     void DrawEntityNode(Entity entity);
+    void DrawComponents(Entity entity);
     Ref<Scene> m_Context{nullptr};
     Entity m_SelectionContext{};
 };
--- gauri_editor/src/gauri_editor/panels/scene_hierarchy_panel.cpp
+++ gauri_editor/src/gauri_editor/panels/scene_hierarchy_panel.cpp
@@ -1,5 +1,7 @@
 #include "gauri_editor/panels/scene_hierarchy_panel.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <imgui.h>
 
 #include "gauri/scene/component.h"
@@ -23,6 +25,18 @@ void SceneHierarchyPanel::OnImGuiRender()
         DrawEntityNode(entity);
     });
 
+    if (ImGui::IsMouseDown(ImGuiMouseButton_Left) && ImGui::IsWindowHovered(ImGuiHoveredFlags_None))
+    {
+        m_SelectionContext = {};
+    }
+
+    ImGui::End();
+
+    ImGui::Begin("Properties");
+    if (m_SelectionContext)
+    {
+        DrawComponents(m_SelectionContext);
+    }
     ImGui::End();
 }
 void SceneHierarchyPanel::DrawEntityNode(Entity entity)
@@ -47,6 +61,31 @@ void SceneHierarchyPanel::DrawEntityNode(Entity entity)
             ImGui::TreePop();
         }
         ImGui::TreePop();
+    }
+}
+void SceneHierarchyPanel::DrawComponents(Entity entity)
+{
+    if (entity.HasComponent<TagComponent>())
+    {
+        auto &tag = entity.GetComponent<TagComponent>().Tag;
+
+        char buffer[256];
+        memset(buffer, 0, sizeof(buffer));
+        strcpy_s(buffer, sizeof(buffer), tag.c_str());
+        if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+        {
+            tag = std::string(buffer);
+        }
+    }
+    if (entity.HasComponent<TransformComponent>())
+    {
+        if (ImGui::TreeNodeEx((void *)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen,
+                              "Transform"))
+        {
+            auto &transform = entity.GetComponent<TransformComponent>().Transform;
+            ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+            ImGui::TreePop();
+        }
     }
 }
 } // namespace gauri
